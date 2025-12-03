ee96a3852e8941e16a2842101fe425b6449197c4
weeger <romain.weeger@gmail.com>
2025-12-03 08:51:44 +0100

    Pubspec

--- src/wexample_wex_addon_dev_flutter/file/flutter_pubspec_yaml_file.py
+++ src/wexample_wex_addon_dev_flutter/file/flutter_pubspec_yaml_file.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 from wexample_filestate.item.file.yaml_file import YamlFile
+from wexample_filestate_git.remote.github_remote import GithubRemote
 from wexample_helpers.decorator.base_class import base_class
 
 
@@ -27,6 +28,33 @@ class FlutterPubspecYamlFile(YamlFile):
 
     def dumps(self, content: dict | None = None) -> str:
         workdir = self.get_parent_item()
+
+        # Always have a dict
+        content = content or {}
+
+        # Always set the package name
         content["name"] = workdir.get_package_name()
 
-        return super().dumps(content or {})
+        # Get repo remote URL (fallback: origin)
+        repo_url = workdir.git_run(cmd=[
+            "remote",
+            "get-url",
+            workdir._get_deployment_remote_name() or "origin"
+        ], capture=True).stdout.strip()
+
+        # Auto-fill URLs if this is a GitHub repo
+        if GithubRemote.is_github_repo(repo_url):
+            resolved_repo_url = GithubRemote.resolve_url_from_repo_url(repo_url)
+
+            if resolved_repo_url:
+                # Fill homepage if missing
+                content.setdefault("homepage", resolved_repo_url)
+
+                # Fill repository if missing
+                content.setdefault("repository", resolved_repo_url)
+
+                # Fill issue tracker if missing
+                content.setdefault("issue_tracker", f"{resolved_repo_url}/issues")
+
+        return super().dumps(content)
+
