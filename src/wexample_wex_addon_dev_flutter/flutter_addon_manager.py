5aa95683913dcbdebfa8a8bd0d1a6901b22ea8bf
weeger <romain.weeger@gmail.com>
2026-08-19 14:36:05 +0200

    Types

--- src/wexample_wex_addon_dev_flutter/flutter_addon_manager.py
+++ src/wexample_wex_addon_dev_flutter/flutter_addon_manager.py
@@ -4,4 +4,19 @@ from wexample_wex_core.common.abstract_addon_manager import AbstractAddonManager
 
 
 class FlutterAddonManager(AbstractAddonManager):
-    pass
+    def get_workdir_types(self) -> dict[str, type]:
+        from wexample_wex_addon_dev_flutter.workdir.flutter_package_workdir import (
+            FlutterPackageWorkdir,
+        )
+        from wexample_wex_addon_dev_flutter.workdir.flutter_packages_suite_workdir import (
+            FlutterPackagesSuiteWorkdir,
+        )
+        from wexample_wex_addon_dev_flutter.workdir.flutter_workdir import (
+            FlutterWorkdir,
+        )
+
+        return {
+            "flutter": FlutterWorkdir,
+            "flutter-package": FlutterPackageWorkdir,
+            "flutter-packages-suite": FlutterPackagesSuiteWorkdir,
+        }
